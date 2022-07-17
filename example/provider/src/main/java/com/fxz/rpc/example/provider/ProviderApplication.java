83be361f5d8349eafa99993dcb411cc697cb9843
fuxiuzhan <fuxiuzhan@163.com>
2022-07-17 11:43:39 +0800

    opts pom

--- example/provider/src/main/java/com/fxz/rpc/example/provider/ProviderApplication.java
+++ example/provider/src/main/java/com/fxz/rpc/example/provider/ProviderApplication.java
@@ -1,12 +1,9 @@
 package com.fxz.rpc.example.provider;
 
 import com.fxz.fuled.service.annotation.EnableFuledBoot;
-import com.fxz.rpc.feign.plus.core.reporter.LogReporter;
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.Import;
 
 @EnableFuledBoot
-@Import(LogReporter.class)
 public class ProviderApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProviderApplication.class, args);
